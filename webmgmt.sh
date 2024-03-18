baf6ab2724c603486a013db5112a6b49f78bc490
bane-frame <145043356+bane-frame@users.noreply.github.com>
2024-03-18 11:17:48 +0100

    added new feature webmgmt.sh
    Added a new feature to see current number of connections on SGA

--- webmgmt.sh
+++ webmgmt.sh
@@ -25,7 +25,8 @@ echo "SGA web management script"
 echo "----------------------"
 echo "1. Enable service"
 echo "2. Disable service"
-echo "3. Exit"
+echo "See current number of connections on SGA"
+echo "4. Exit"
 
 read -p "Enter your choice: " choice
 
@@ -39,12 +40,15 @@ case $choice in
         disable_service app_mgmt_web_secure.service
         ;;
     3)
+        watch -d 'echo -e "FRP8 connections:" ; count8=`sudo netstat -anp |grep turn|grep -v 127|grep udp|uniq|wc -l` ; count81=$(($count8-2)) ; echo $count81 ; echo -e "FRP7 connections:" ; count7=`sudo netstat -an |grep :443 |grep EST|uniq|wc -l` ; count71=$(($count7/2));echo $count71; echo "CTRL+C to exit"'
+        ;;
+    4)  
         # goto end
         echo "Exiting the script"
         exit 0
         ;;
     *)
-        echo "Invalid choice. Please choose 1, 2, or 3."
+        echo "Invalid choice. Please choose 1, 2, 3 or 4."
         ;;
 esac
 # end of script
