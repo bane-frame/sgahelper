f23a9025b91c44d7255592dce2fbc50989adbbe5
bane-frame <branislav.drobac@dizzion.com>
2024-03-23 17:30:13 +0100

    code cleanup

--- sgahelp.sh
+++ sgahelp.sh
@@ -4,9 +4,9 @@
 #                       SGA Helper v1.6.6                  #
 ############################################################
 
-# begin of service management functions
+### begin of service management functions ###
 # function to enable service
-enable_service() {
+function enable_service() {
     echo "Enabling the service..."
     systemctl enable "$1"
     systemctl start "$1"
@@ -14,7 +14,7 @@ enable_service() {
 }
 
 # function to disable service
-disable_service() {
+function disable_service() {
     echo "Disabling the service..."
     systemctl stop "$1"
     systemctl disable "$1"
@@ -22,33 +22,33 @@ disable_service() {
 }
 
 # function to check service status
-service_status() {
+function service_status() {
     state=$(systemctl is-active "$1")
         if echo "$state" | grep active > /dev/null 2>&1;
             then echo "RUNNING"
             else echo "DOWN"
         fi
 }
-# end of service management functions
+### end of service management functions ###
 
-# protocol statistics functions
+### protocol statistics functions ###
 # function for FRP7
-FRP7() {
-count7=`sudo netstat -an |grep :443 |grep EST|uniq|wc -l`
+function FRP7() {
+count7=`sudo netstat -an | grep :443 | grep EST | uniq | wc -l`
 count71=$(($count7/2))
 echo $count71
 }
 
 # function for FRP8
-FRP8() {
-count8=`sudo netstat -anp |grep turn|grep -v 127|grep udp|uniq|wc -l`
+function FRP8() {
+count8=`sudo netstat -anp | grep turn | grep -v 127 | grep udp | uniq | wc -l`
 count81=$(($count8-2))
 echo $count81
 }
-# end of protocol statistics
+### end of protocol statistics ###
 
-# main menu function
-main_menu() {
+### main menu function ###
+function main_menu() {
 sp=$(service_status app_mgmt_web.service)
 sf7=$(service_status nginx.service)
 sf8=$(service_status coturn.service)
@@ -56,6 +56,7 @@ clear
 echo "SGA Helper v1.6.6"
 echo "----------------------------------"
 echo " "
+# health dashboard
 echo "SGA services health:"
 echo " "
 echo "SGA status page is         [$sp]"
@@ -64,6 +65,7 @@ echo "Coturn/FRP8 protocol is    [$sf8]"
 echo " "
 echo "----------------------------------"
 echo " "
+# choice
 echo "1. Enable status page service"
 echo "2. Disable status page service"
 echo "3. Connection statistics on SGA"
@@ -93,26 +95,28 @@ case $choice in
         main_menu
         ;;
     3)
-        # see the current number of connections to SGA
         clear
-        echo -e "FRP8 connections:" ; FRP8
-        echo -e "FRP7 connections:" ; FRP7
+        # see the current number of connections to SGA
+        echo "FRP8 (UDP only) connections:" ; FRP8
+        echo "FRP7 connections:" ; FRP7
         read -p "Press enter to go back on main menu"
         main_menu
         ;;
     4)  
-        # test communication with stun.console.nutanix.com
         clear
-        echo -e 'Expected result is the public IP address of SGA'
-        echo -e 'which should be same as public IP associated with FQDN'
+        echo "Expected result is the public IP address of SGA"
+        echo "which should be same as public IP associated with FQDN"
+        echo " "
+        # test communication with stun.console.nutanix.com
         /usr/local/bin/external_ip_via_stun.sh stun.console.nutanix.com
+        echo " "
         read -p "Press enter to go back on main menu"
         main_menu
         ;;
     5)  
-        # test nginx configuration
         clear
-        echo -e 'Testing NGINX configuration...'
+        echo "Testing NGINX configuration..."
+        # size check
         filesize=$(stat -c%s "/etc/nginx/nginx.conf")
         echo "Size of configuration file is $filesize bytes."
             if (( filesize > 0 )); then
@@ -129,31 +133,32 @@ case $choice in
         main_menu
         ;;
     6)  
-        # show nginx errors
         clear
+        # show nginx errors
         tail /var/log/nginx/error.log
         read -p "Press enter to go back on main menu"
         main_menu
         ;;
     7)  
-        # force log rotation to cleanup /var/log partition
         clear
-        sudo du -sh /var/log/* |sort -k 1 -n -r
-        echo -e "==================== FORCING LOG ROTATION ==============================="
+        echo "Showing current usage per file:"
+        sudo du -sh /var/log/* | sort -k 1 -n -r
+        echo "=================== FORCING LOG ROTATION ==================="
+        # force log rotation to cleanup /var/log partition
         sudo find /etc/logrotate.d/ -type f -name '*' -print0 | sudo xargs -0 logrotate -f -d
         read -p "Press enter to go back on main menu"
         main_menu
         ;;
     8)  
-        # make backup of coturn unit
         clear
+        # make backup of coturn unit
         sudo cp /etc/systemd/system/coturn.service /home/nutanix/coturn.service.bak
         # remove nonce mechanism from coturn configuration
         sudo sed -i '/nonce/d' /etc/systemd/system/coturn.service
         # restart coturn service with new configuration
         sudo systemctl daemon-reload
         sudo systemctl restart coturn
-        # check service status
+        # check service status if fails revert backup
         STATUS="$(systemctl is-active coturn.service)"
             if [ "${STATUS}" = "inactive" ]; then
                 echo "Service not running as expected, reverting backup"
@@ -177,7 +182,7 @@ case $choice in
         main_menu
         ;;
 esac
-# end of main menu fuction
+### end of main menu fuction ###
 }
 # begin
 main_menu
